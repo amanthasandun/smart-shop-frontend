b3920bcfbed623bd4baa882631fd7f1fb537446b
amanthasandun <amanthasanduna@gmail.com>
2026-03-13 22:20:41 +0530

    create the login form and plug into the home page 🔥🔥🔥

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import { Route, Router, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer.jsx'
+import { useAppContext } from './context/AppContext.jsx'
+import Login from './components/Login.jsx'
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller")
+  const {showUserLogin} = useAppContext()
 
   return (
     <div>
       {isSellerPath ?null : <Navbar/>}
+      {showUserLogin ? <Login/> : null}
       <Toaster/>
       <div className={`$isSellerPath ? "" : 'px-6 md:px-16 lg:px-24 xl:px-32'`}>
         <Routes>
