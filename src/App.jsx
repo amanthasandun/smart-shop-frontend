5e9c217d2b2fe0f786dd851d225b159c74ebe9cb
amanthasandun <amanthasanduna@gmail.com>
2026-03-21 16:27:44 +0530

    create the sellerlayout page

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,17 @@ import ProductDetail from './pages/ProductDetail.jsx'
 import Cart from './pages/Cart.jsx'
 import AddAddress from './pages/AddAddress.jsx'
 import MyOrders from './pages/MyOrders.jsx'
+import SellerLogin from './components/seller/SellerLogin.jsx'
+import SellerLayout from './pages/seller/SellerLayout.jsx'
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller")
-  const {showUserLogin} = useAppContext()
+  const {showUserLogin , isSeller} = useAppContext()
 
   return (
-    <div>
-      {isSellerPath ?null : <Navbar/>}
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
+      {isSellerPath ? null : <Navbar/>}
       {showUserLogin ? <Login/> : null}
       <Toaster/>
       <div className={`$isSellerPath ? "" : 'px-6 md:px-16 lg:px-24 xl:px-32'`}>
@@ -29,9 +31,12 @@ const App = () => {
           <Route path="/products" element={<AllProducts/>}/>
           <Route path="/products/:category" element={<ProductCatergory/>}/>
           <Route path="/products/:category/:id" element={<ProductDetail/>}/>
-          <Route path="/Cart" element={<Cart/>}/>
+          <Route path="/cart" element={<Cart/>}/>
           <Route path="/add-address" element={<AddAddress/>}/>
           <Route path="/my-orders" element={<MyOrders/>}/>
+          <Route path="/seller" element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+            
+          </Route>
 
         </Routes>
       </div>
